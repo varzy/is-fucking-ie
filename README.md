061d72f4127076bdc77a23a5cab5e610f3118618
zy <s@zyis.me>
2017-12-15 15:00:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# is-fucking-ie
+
+作者在被 IE 深深伤害后的作品。他发誓他的个人项目从此以后都将拒绝来自 IE 的访问，并且他单方面得深信这种行为将使世界变得更加美好。
+
+This is a project made by the programmer after getting hurt by IE. He swore that he would refuse the visits from IE on his personal projects and believed that this action would make the world better.
--- is-fucking-ie.js
+++ is-fucking-ie.js
@@ -1,9 +1,11 @@
-function isFuckingIe (url = 'https://www.google.com/intl/en/chrome/browser/desktop/') {
+/**
+ * thanks EpokK & Michel Floyd
+ * link: https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
+ */
+(function isFuckingIe () {
   if (navigator.userAgent.indexOf('MSIE')!==-1
   || navigator.appVersion.indexOf('Trident/') > 0) {
     alert('Please don\'t use the fucking ie!')
-    window.location.href = url
+    window.location.href = 'https://www.google.com/intl/en/chrome/browser/desktop/'
   }
-}
-
-export default isFuckingIe
+} ())
